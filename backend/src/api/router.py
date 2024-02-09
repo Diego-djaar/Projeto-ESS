bf0ae2b19b925b7f4593dea59d58afa8a39b6bbc
Breno <brenomelogabriel2019@gmail.com>
2024-02-09 14:01:22 -0300

    feat: adding the route of the payment method feature

--- backend/src/api/router.py
+++ backend/src/api/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from src.api import items
+from src.api import items, payment_methods
 
 api_router = APIRouter()
 api_router.include_router(items.router, prefix="/items", tags=["items"])
+api_router.include_router(payment_methods.router, prefix="/payment")
