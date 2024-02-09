ef72c5af00718c3a5a6b52b66e1e876c885de0a8
Breno <brenomelogabriel2019@gmail.com>
2024-02-09 11:06:31 -0300

    fix: removing the Pix schema and renaming the Boleto schema

--- backend/src/schemas/payment_schema.py
+++ backend/src/schemas/payment_schema.py
@@ -1,17 +1,12 @@
 from pydantic import BaseModel
 import datetime
 
-class Boleto(BaseModel): 
+class Boleto_pix(BaseModel): 
     type: str
     nome_completo: str
     cpf: str
 
-class Pix(BaseModel): 
-    type: str
-    nome_completo: str
-    cpf: str
-
-class Cartao(Boleto):
+class Cartao(Boleto_pix):
     cvv: str
     nome_cartao: str
     num_cartao: str
