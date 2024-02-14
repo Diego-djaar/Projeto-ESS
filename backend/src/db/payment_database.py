2ecba2cab76ec589d69f7bd567e61cc3ee273fc6
Breno <brenomelogabriel2019@gmail.com>
2024-02-13 21:22:04 -0300

    feat(payment_methods.py) Adicionando uma base de dados para os métodos de pagamento

---
+++ backend/src/db/payment_database.py
@@ -0,0 +1,37 @@
+import json 
+import datetime 
+import re 
+
+cpf_pattern = re.compile(r"^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}$")
+cartao_pattern = re.compile(r"^5[1-5][0-9]{14}$")
+
+database = {} 
+
+def escrever_arquivo(database): 
+    with open("payment_database.json", "w") as f: 
+        json.dump(database, f, default=str, indent=4)
+
+def ler_arquivo(database): 
+    with open("payment_database.json", "r") as f:
+        return json.load(f)
+
+def Adicionar_cartao(nome_cartao: str, numero_cartao: str, cvv: str, cpf: str, validade: datetime.date): 
+    #if not cpf_pattern.match(cpf):
+        #raise ValueError("CPF inválido")
+
+    #if not cartao_pattern.match(numero_cartao):
+        #raise ValueError("Número de cartão inválido")
+
+    if cpf not in database:
+        database[cpf] = []
+
+    cartao = {
+        "nome_cartao": nome_cartao,
+        "numero_cartao": numero_cartao,
+        "cvv": cvv,
+        "validade": validade.strftime("%Y-%m-%d"),
+        "cpf": cpf
+    }
+
+    database[cpf].append(cartao)
+    escrever_arquivo(database)
