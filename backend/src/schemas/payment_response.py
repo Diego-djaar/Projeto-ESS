95eaaa9c83daede9ffe2376a4c6835d04c27d384
Breno <brenomelogabriel2019@gmail.com>
2024-02-14 14:32:42 -0300

    feat: adding the BAD_REQUEST method

--- backend/src/schemas/payment_response.py
+++ backend/src/schemas/payment_response.py
@@ -10,3 +10,33 @@ class HTTPPaymentResponse:
             message="Payment method has been inserted sucessfully",
             status_code=201
         )
+    
+    # @staticmethod
+    # def INVALID_CPF() -> HttpResponseModel:
+    #     return HttpResponseModel (
+    #         message="Invalid CPF",
+    #         status_code=400
+    #     )
+    
+    # @staticmethod
+    # def EXPIRED_DATA() -> HttpResponseModel:
+    #     return HttpResponseModel (
+    #         message="The card is expired",
+    #         status_code=400
+    #     )
+    
+    # @staticmethod
+    # def INVALID_NUMBER() -> HttpResponseModel:
+    #     return HttpResponseModel (
+    #         message="The card number is invalid", 
+    #         status_code=400 
+    #     )
+    
+    @staticmethod
+    def BAD_REQUEST(problemas) -> HttpResponseModel:
+
+        return HttpResponseModel (
+            message="Os campos a seguir estão preenchidos incorretamente", 
+            status_code=400, 
+            data= problemas
+        )
