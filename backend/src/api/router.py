8229a83c506468314d487634f436a5a08757949b
Breno <brenomelogabriel2019@gmail.com>
2024-02-14 15:34:20 -0300

    fix: adding a tag to payment methods

--- backend/src/api/router.py
+++ backend/src/api/router.py
@@ -3,4 +3,4 @@ from src.api import items, payment_methods
 
 api_router = APIRouter()
 api_router.include_router(items.router, prefix="/items", tags=["items"])
-api_router.include_router(payment_methods.router, prefix="/payment")
+api_router.include_router(payment_methods.router, prefix="/backend/src/api/payment", tags=["Payment"])
