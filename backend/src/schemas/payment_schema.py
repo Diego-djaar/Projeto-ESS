3a0514fea758f9de0fc37b7da8c478ca6e953f47
Breno <brenomelogabriel2019@gmail.com>
2024-02-14 18:31:36 -0300

    feat: changing the user identifier from CPF to username

--- backend/src/schemas/payment_schema.py
+++ backend/src/schemas/payment_schema.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 import datetime
 
 class Cartao(BaseModel): 
+    user_name: str
     nome_cartao: str 
     numero_cartao: str 
     cvv: str 
@@ -9,10 +10,12 @@ class Cartao(BaseModel):
     validade: datetime.date
 
 class Pix(BaseModel):
+    user_name: str
     nome_completo: str 
     cpf: str 
 
 class Boleto(BaseModel):
+    user_name: str
     nome_completo: str 
     cpf: str 
 
