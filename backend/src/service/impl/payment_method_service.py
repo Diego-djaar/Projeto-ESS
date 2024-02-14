9f0fd543228871974e6d63cc80a60d2b0e439192
Breno <brenomelogabriel2019@gmail.com>
2024-02-13 21:42:15 -0300

    fix(service): fixing the inserting_method of the PaymentService class

--- backend/src/service/impl/payment_method_service.py
+++ backend/src/service/impl/payment_method_service.py
@@ -1,10 +1,13 @@
-from src.db.__init__ import database as db
-from src.schemas.payment_schema import Boleto_pix
+from src.db.payment_database import Adicionar_cartao
+from src.schemas.payment_schema import Cartao
+from src.schemas.response import HTTPResponses, HttpResponseModel
 
-
-class payment_service:
+class PaymentService:
 
     @staticmethod
-    def inserting_method(boleto: Boleto_pix):
-        result = db.add("payments", boleto)
-        return result 
+    def inserting_method(cartao: Cartao) -> HttpResponseModel:
+
+        consulta = Adicionar_cartao(*cartao.model_dump().values())
+
+        if consulta: 
+            return HTTPResponses.ITEM_CREATED
