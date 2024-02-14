d78128eb1c36c5541b35d9936777453b76beddf0
Breno <brenomelogabriel2019@gmail.com>
2024-02-13 21:32:02 -0300

    feat(schemas) Adicionando um schema para o cartao

--- backend/src/schemas/payment_schema.py
+++ backend/src/schemas/payment_schema.py
@@ -1,20 +1,14 @@
 from pydantic import BaseModel
 import datetime
 
-class PaymentBase(BaseModel): 
-    nome_completo: str
-    cpf: str
-
-class Pix(PaymentBase):
-    pass 
-
-class Boleto(PaymentBase):
-    pass
-
-class Cartao(PaymentBase):
-    cvv: str
-    nome_cartao: str
-    num_cartao: str
+class cartao(BaseModel): 
+    cpf: str 
+    nome_cartao: str 
+    numero_cartao: str 
+    cvv: str 
     validade: datetime.date
+
+
+
 
 
