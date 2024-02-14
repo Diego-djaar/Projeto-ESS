a5876ff5bce48e31979309e9ff4672078a84d6f6
Breno <brenomelogabriel2019@gmail.com>
2024-02-14 15:40:42 -0300

    feat: adding the '/inserting/pix' and '/inserting/boleto' endpoints

--- backend/src/api/payment_methods.py
+++ backend/src/api/payment_methods.py
@@ -10,16 +10,42 @@ router = APIRouter()
 @router.post(
         '/inserting/cartao', 
         response_model=HttpResponseModel,
-        status_code=status.HTTP_201_CREATED, 
         summary="Create a new payment method", 
              )
 def insert_payment(cartao: Cartao) -> HttpResponseModel: 
-    response = PaymentService.inserting_method(cartao)
+    response = PaymentService.inserting_cartao(cartao)
     return response 
 
 @router.post(
-    "/inserting/{method_name}",
-    summary="Inserting a new payment method"
-)
-def insert_payment_method(method_name: str, cartao: Cartao, pix: Pix, boleto: Boleto) -> HttpResponseModel:
-    pass
+        '/inserting/pix', 
+        response_model=HttpResponseModel,
+        summary="Create a new payment method", 
+             )
+def insert_payment(pix: Pix) -> HttpResponseModel: 
+    response = PaymentService.insertion_pix(pix)
+    return response 
+
+@router.post(
+        '/inserting/boleto', 
+        response_model=HttpResponseModel,
+        summary="Create a new payment method", 
+             )
+def insert_payment(boleto: Boleto) -> HttpResponseModel: 
+    response = PaymentService.insertion_boleto(boleto)
+    return response 
+
+# @router.post(
+#     "/inserting/{method_name}",
+#     response_model=HttpResponseModel,
+#     summary="Create a new payment method"
+# )
+# def insert_payment_method(method_name: str, cartao: Cartao, pix: Pix, boleto: Boleto) -> HttpResponseModel:
+#     if method_name == "boleto": 
+#         response = PaymentService.inserting_cartao(boleto)
+#         return response 
+#     elif method_name == "pix": 
+#         response = PaymentService.insertion_pix(pix)
+#         return response 
+#     elif method_name == "cartao":
+#         response = PaymentService.inserting_cartao(cartao)
+#         return response 
