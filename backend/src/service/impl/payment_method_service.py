2458ab9b32652b329930109258e71c168d95b8f4
Breno <brenomelogabriel2019@gmail.com>
2024-02-14 15:36:54 -0300

    feat: adding the insertion_boleto and insertion_pix methods

--- backend/src/service/impl/payment_method_service.py
+++ backend/src/service/impl/payment_method_service.py
@@ -1,12 +1,12 @@
-from src.db.payment_database import Adicionar_cartao
-from src.schemas.payment_schema import Cartao
+from src.db.payment_database import Adicionar_cartao, Adicionar_pix, Adicionar_boleto
+from src.schemas.payment_schema import Cartao, Pix, Boleto 
 from src.schemas.response import HTTPResponses, HttpResponseModel
 from src.schemas.payment_response import HTTPPaymentResponse
 
 class PaymentService:
 
     @staticmethod
-    def inserting_method(cartao: Cartao) -> HttpResponseModel:
+    def inserting_cartao(cartao: Cartao) -> HttpResponseModel:
 
         sucesso, problemas = Adicionar_cartao(*cartao.model_dump().values())
 
@@ -18,3 +18,17 @@ class PaymentService:
         #     return HTTPPaymentResponse.EXPIRED_DATA()
         # elif "CARD_NUMBER" in problemas:
         #     return HTTPPaymentResponse.INVALID_NUMBER()
+    
+    @staticmethod
+    def insertion_pix(pix: Pix) -> HttpResponseModel: 
+
+        Adicionar_pix(*pix.model_dump().values())
+
+        return HTTPPaymentResponse.PIX_INSERTED_SUCESSFULLY()
+    
+    @staticmethod
+    def insertion_boleto(boleto: Boleto) -> HttpResponseModel: 
+
+        Adicionar_boleto(*boleto.model_dump().values())
+
+        return HTTPPaymentResponse.BOLETO_INSERTED_SUCESSFULLY()
