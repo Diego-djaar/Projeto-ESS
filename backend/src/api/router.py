d9afc8aeed758e96b2de69fbff1be350e9b48659
u <pco2@cin.ufpe.br>
2024-02-20 19:58:11 -0300

    feat: add inventory router

--- backend/src/api/router.py
+++ backend/src/api/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from src.api import carrinho, users, recuperation
+from src.api import carrinho, users, recuperation, inventory
 
 api_router = APIRouter()
 
 api_router.include_router(carrinho.router, prefix="/carrinho", tags = ["carrinho"])
 api_router.include_router(users.router, prefix="/auth/user", tags = ["user"])
 api_router.include_router(recuperation.router, prefix="/esqueciasenha", tags = ["recuperation"])
+api_router.include_router(inventory.router, prefix="/inventory", tags = ["inventory"])
