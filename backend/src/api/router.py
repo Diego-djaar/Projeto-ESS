ab2bfaa649e2ea056154407e52697e45e2714dae
VmtpCin <108680288+VmtpCin@users.noreply.github.com>
2024-02-20 15:58:05 -0300

    Features merged with development, working fine

--- backend/src/api/router.py
+++ backend/src/api/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from src.api import users, recuperation
+from src.api import users, recuperation, stores
 
 api_router = APIRouter()
 # api_router.include_router(items.router, prefix="/items", tags=["items"])
 
 api_router.include_router(users.router, prefix="/auth/user", tags = ["user"])
 api_router.include_router(recuperation.router, prefix="/esqueciasenha", tags = ["recuperation"])
+api_router.include_router(stores.router, prefix="/stores")
