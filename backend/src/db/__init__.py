8a09ec04ff6346e0c81c16452862ea4418a9ade7
‘Diego’ <djaar@cin.ufpe.br>
2024-02-20 15:20:33 -0300

    Remove: unused files from base project

--- backend/src/db/__init__.py
+++ backend/src/db/__init__.py
@@ -1,9 +1,6 @@
-from .database import Database
 from .user_database import UserDatabase
 from .codigos_rec_database import RecuperacaoDatabase
-from .config.create_collections import create_collections
 
-database = Database()
 user_database = UserDatabase()
 user_database_example = UserDatabase("Usuários teste.json")
 user_database_example.clear_database()
