79ea8fdad9b746fd66f726e9dce4b166b1477ebe
Joao Felipe Sobral <jfmvs@hotmail.com>
2024-02-21 05:03:23 -0300

    Testes Histórico

--- backend/src/api/router.py
+++ backend/src/api/router.py
@@ -3,4 +3,4 @@ from src.api import items, order_history
 
 api_router = APIRouter()
 api_router.include_router(items.router, prefix="/items", tags=["items"])
-api_router.include_router(order_history.router, prefix="/orders", tags=["orders"])
+api_router.include_router(order_history.router, prefix="/Orders", tags=["orders"])
