79d3e871e6767c8fd02e0c933cfefec8d6772112
Breno <brenomelogabriel2019@gmail.com>
2024-03-08 00:26:36 -0300

    feat: the 'InsertingBoleto' page now can comunicate with API

--- frontend/src/app/home/components/Formulário/index.tsx
+++ frontend/src/app/home/components/Formulário/index.tsx
@@ -1,10 +1,16 @@
 import styles from "./index.module.css";
 
-const Formulario = ({ label }) => {
+const Formulario = ({ paymentType}) => {
   return (
-    <div className={styles.forms}>
-      <label className={styles.label}>{label}</label>
-      <input className = {styles.caixaTexto} type="text" />
+    <div>
+      <form className={styles.forms}>
+      <label className={styles.label}>Nome completo</label>
+      <input className = {styles.caixaTexto} type="text" placeholder="Digite aqui o seu nome completo"/>
+      <label className={styles.label}>CPF</label>
+      <input className = {styles.caixaTexto} type="text" placeholder="Digite aqui o seu CPF"/>
+      <br />
+      <input type="Submit" value={paymentType} className={styles.submissionButton}/>
+    </form>
     </div>
   );
 };
--- frontend/src/app/home/pages/InsertingBoleto/index.tsx
+++ frontend/src/app/home/pages/InsertingBoleto/index.tsx
@@ -1,19 +1,65 @@
 import styles from "./index.module.css";
-import Button from "../../components/buttom";
+// import Button from "../../components/buttom";
 import Pichu from "../../components/logo";
 import Message from "../../components/mensage";
-import Formulario from "../../components/Formulário";
+// import Formulario from "../../components/Formulário";
 import { useNavigate } from 'react-router-dom';
-
+import {useState, useEffect} from 'react'
+import axios from "axios";
+// import axios from "axios";
 
 const InsertingBoleto = () => {
+
+    const [nome, setNomeCompleto] = useState("");
+    const [cpf, setCpf] = useState("");
+    const navigate = useNavigate(); 
+
+    const insertBoletoHandler = (event) => {
+
+        event.preventDefault();
+
+        console.log(typeof nome)
+        console.log(typeof cpf)
+
+        axios.post("http://127.0.0.1:8000/payment/inserting/pix", 
+        {
+            "nome_completo": nome,
+            "cpf": cpf
+        })
+        .then(res => {if (res.data.message == "metodo de pagamento cadastrado com sucesso") {
+
+            navigate("/paymentMethod/inserting")
+            alert("Informações cadastradas com sucesso")
+        }else if (res.data.message == "Já existe um pix cadastrado no sistema"){
+
+            alert(res.data.message)
+
+        }else {
+
+            alert("Informações inválidas")
+
+        } } )
+
+
+    }
 
     return (
         <div>
             <Pichu></Pichu>
-            <Message value="preencha com as suas informações "></Message>
-            <Formulario label="Nome completo"></Formulario>
-            <Formulario label="CPF"></Formulario>
+            <Message value="preencha com as suas informações"></Message>
+            <div>
+            <form className={styles.forms}>
+            <label className={styles.label}>Nome completo</label>
+            <input className = {styles.caixaTexto} type="text" placeholder="Digite aqui o seu nome completo" onChange={event => 
+            setNomeCompleto(event.target.value)}/>
+            <label className={styles.label}>CPF</label>
+            <input className = {styles.caixaTexto} type="text" placeholder="Digite aqui o seu CPF" onChange={event => 
+            setCpf(event.target.value)}/>
+            <br />
+            <button  className={styles.submissionButton} onClick = {event => insertBoletoHandler(event)}>Inserir boleto</button>
+            </form>
+            </div>
+    
         </div>
     )
 
