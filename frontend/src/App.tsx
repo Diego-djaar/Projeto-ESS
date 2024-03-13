cacee68b302aa26b01354785e71a3025b1ecc800
VmtpCin <108680288+VmtpCin@users.noreply.github.com>
2024-03-13 00:53:09 -0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -68,7 +68,6 @@ const router = createBrowserRouter([
     path: "/stores/retrieve_password",
     Component: StoreRetrieve,
     
->>>>>>> development
   },
   {
     path: "/stores/update_store",
@@ -98,29 +97,4 @@ class App extends Component {
     }
 }
 export default App;
-
-
-class App extends Component {
-    props: {page_atual: string}
-    constructor(props: {page_atual: string}) {
-        super(props);
-        this.props = props;
-        this.state = { page_atual: page_atual }
-        this.reload = this.reload.bind(this)
-        refresh_window_assign(this.reload)
-    }
-
-    reload() {
-      window.location.href = '/' + page_atual;
-      this.setState({
-          page_atual: page_atual
-      })
-    }
-
-    render() {
-        return <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />;
-    }
-}
-export default App;
-
 
